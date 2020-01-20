58e9d3b139cc75137aefe54d96a72fb88dce088c
Tim Golden <mail@timgolden.me.uk>
2020-01-20 10:28:11 +0000

    Use matplotlib to draw a graph of the data

--- workshop.py
+++ workshop.py
@@ -1,8 +1,13 @@
 #!python3
 import csv
 from collections import defaultdict
-import operator
 from pprint import pprint
+
+#
+# Read listener data into a list of dictionaries
+# Each dictionary in the list represents one row in the file
+# Each item in the dictionary represents one named column in that row
+#
 
 with open("listeners.csv", newline="") as f:
     reader = csv.DictReader(f)
@@ -10,8 +15,16 @@ with open("listeners.csv", newline="") as f:
 
 pprint(listeners)
 
-dimension_name = "Station"
-measure_name = "Hours"
+#
+# For a selected dimension (Brand, Station etc.) add up all
+# of a selected value (Hours, Sessions etc.)
+# The result is a dictionary where each key represents
+# one item in the dimension; and each value represents
+# the sum of all the corresponding values
+#
+
+dimension_name = "Brand"
+measure_name = "Sessions"
 
 summary_data = defaultdict(int)
 for listener in listeners:
@@ -21,5 +34,21 @@ for listener in listeners:
 
 pprint(summary_data)
 
+#
+# Show, in text form, the summary just created
+#
+
 for station, hours in summary_data.items():
     print(station, "=>", hours)
+
+#
+# Show the same summary on a horizontal bar chart
+#
+import matplotlib.pyplot as plt
+
+labels = list(summary_data.keys())
+values = list(summary_data.values())
+bar_chart = plt.barh(labels, values)
+plt.ylabel(dimension_name)
+plt.xlabel(measure_name)
+plt.show()
